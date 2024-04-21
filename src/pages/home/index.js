797d15d213bff6cb9944c4dc50cf2a914eaf12b1
WCNB123456 <167688353+WCNB123456@users.noreply.github.com>
2024-04-21 20:43:18 +0800

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,7 @@ const Home =()=>{
     return(
         <div className="bg">
             <div className="title">
-                需要生成密码？试试Meinaの密码生成器
+                需要生成密码？试试变幻密码盒
             </div>
             <div className="sub-title">
                 生成安全、随机的密码以保持网络安全
